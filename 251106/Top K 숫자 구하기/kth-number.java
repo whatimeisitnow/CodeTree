ee10561293cc41fa0723c0e648a790b546b68902
sujin <jayjay.hi.77@gmail.com>
2025-11-06 17:05:11 +0900

    Title: Top K 숫자 구하기, Time: 169ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 251106/Top K 숫자 구하기/kth-number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        Arrays.sort(nums);
+        System.out.println(nums[k-1]);
+    }
+}
